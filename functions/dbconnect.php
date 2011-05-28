1f30fb04bd7b2e79e76b4915baccc64b94f3664f
Vipin Nair <swvist@gmail.com>
2011-05-28 20:59:15 +0530

    Added documentation to dbconnect.php.

--- functions/dbconnect.php
+++ functions/dbconnect.php
@@ -1,16 +1,34 @@
 <?php
-	function dbconnect()
-	{
-		$dbconn = pg_pconnect('host=localhost port=5432 dbname=southpark user=serveruser password=DBPa$$word');
-		return $dbconn;
-	}
-	function dbquery($sql)
-	{
-		dbconnect();
-		return pg_query($sql);
-	}
-	function dbclose($dbconn)
-	{
-		pg_close($dbconn);
-	}
+/**
+* Database Connection Functions.
+* Any function required to connect to the database should connect to through these functions ONLY.
+* @package database
+*/
+
+$host='localhost';
+$port='5432';
+$dbname='southpark';
+$user='serveruser';
+$password='DBPa$$word';
+
+/**
+* Function to connect to the database. Creates a persistent connection.
+* @return resource Database connection resource.
+*/
+function dbconnect()
+{
+	$dbconn = pg_pconnect('host=localhost port=5432 dbname=southpark user=serveruser password=DBPa$$word');
+	return $dbconn;
+}
+
+/**
+* Function to query database.
+* @param string $sql A sql query. Any variable in sql statement MUST be escaped.
+* @return result_resource Result resource.
+*/
+function dbquery($sql)
+{
+	dbconnect();
+	return pg_query($sql);
+}
 ?>
