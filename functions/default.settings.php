5d8a68de6cb3d2084eb1040e4f02c8b05da6e863
Vipin Nair <swvist@gmail.com>
2011-06-01 18:31:32 +0530

    Added default setting files default.settings.php. Default database settings file functions/db.php will be purged for the repository.

---
+++ functions/default.settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Default settings of the application are set here. Set the variables with appropriate
+* values accordingly and RENAME the file from default.settings.php to settings.php
+* @package settings
+*/
+
+$host		=	''; 		// <Database Host>
+$port		=	''; 		// <Database Port>
+$dbname		=	''; 	// <Database Name>
+$user		=	''; 		// <Database User>
+$password	=	''; 	// <Database Password>
+
+?>
