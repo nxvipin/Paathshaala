b64dafab916605db3a1fd2310d0ac623d549f97b
Vipin Nair <swvist@gmail.com>
2011-06-02 21:03:32 +0530

    Implemented pseudo constructor in class, create() function defined to add video data to the database, documented the existing functions.

--- functions/class.video.php
+++ functions/class.video.php
@@ -20,34 +20,75 @@ class video extends content
 {
 	protected $serverid, $path, $file;
 	
-	public function __construct($cid)
+	/**
+	* The constructor selects the appropriate function based on the number of
+	* arguments and calls the appropriate protected function.
+	*/
+	public function __construct()
+	{
+		$a = func_get_args();
+        $i = func_num_args(); 
+        if($i==1)
+            call_user_func_array(array($this,'view'),$a);
+		if($i==6)
+			call_user_func_array(array($this,'create'),$a);
+	}
+	
+	/**
+	* Function is called to initialize the properties with the given $cid.
+	* the protected property cid with the argument and calls the
+	* getDetails() method to initialize other class properties from the database.
+	* @param integer $cid Content ID
+	*/
+	public function view($cid)
 	{
 		$this->cid=$cid;
 		$this->getDetails();
 	}
-	protected function getDetails()
+	
+	/**
+	* Takes the content data (Video data) as arguments and pushes it to the database.
+	* Object property $cid is initialized after data is inserted.
+	* @param string $title Title of the video.
+	* @param string $desc Description of the video.
+	* @param integer $uid User ID who submitted the video.
+	* @param integer $sid Server ID of the video where it is stored. (SP for southpark)
+	* @param string $path Location of the video file WITHOUT any trailing slashes.
+	* @param string $file File name of the video WITHOUT extension.
+	*/
+	public function create($title,$desc,$uid,$sid,$path,$file)
 	{
-		$sql="Select * from content_video where cn_id='".$this->cid."'";
-		$res=dbquery($sql);
-		$vid=pg_fetch_assoc($res);
-		$this->title=$vid['cn_title'];
-		$this->desc=$vid['cn_desc'];
-		$this->timestamp=$vid['cn_timestamp'];
-		$this->status=$vid['cn_status'];
-		$this->views=$vid['cn_views'];
-		$this->uid=$vid['cn_uid'];
-		$this->serverid=$vid['cf_serverid'];
-		$this->path=$vid['cf_path'];
-		$this->file=$vid['cv_file'];
+		$this->title=pg_escape_string($title);
+		$this->desc=pg_escape_string($desc);
+		$this->uid=pg_escape_string($uid);
+		$this->sid=pg_escape_string($sid);
+		$this->path=pg_escape_string($path);
+		$this->file=pg_escape_string($file);
+		$sql="INSERT INTO content_video(cn_title, cn_desc, cn_userid, cf_serverid, cf_path, cv_file) VALUES('".$this->title."','".$this->desc."','".$this->uid."','".$this->sid."','".$this->path."','".$this->file."') Returning cn_id";
+		$row=pg_fetch_row(dbquery($sql));
+		$this->cid=$row[0];
 	}
+	
+	/**
+	* Returns the complete path of the video file. 
+	* Path is of the form: http://<server_ip>/<path>/<cid><file>.ogv
+	*/
 	public function getCompletePath()
 	{
 		return getServer($this->serverid)."/".$this->path."/".$this->cid.$this->file.".ogv";
 	}
+	
+	/**
+	* Returns the complete path of the video file. 
+	* Path is of the form: http://<server_ip>/thumbs/<cid><file>.png
+	*/
 	public function getThumbnail()
 	{
 		return getServer($this->serverid)."/thumbs/".$this->cid.$this->file.".png";
 	}
+	
+	
+	
 	
 	
 }
