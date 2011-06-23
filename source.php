d92e63345a4e6a256a8f3973a7562b6b32b0c573
Jaseem Abid <jaseemabid@gmail.com>
2011-06-24 03:25:22 +0530

    Added $header with all existing js files alone. This will be included in all pages as header

---
+++ source.php
@@ -0,0 +1,13 @@
+<?php
+
+$header="
+	<meta charset='UTF-8' />
+	<script src='js/jquery-1.6.1.min.js' type='text/javascript'></script>
+	<script src='js/jquery.timeago.js' type='text/javascript'></script>
+	<script src='js/grayout.js' type='text/javascript' ></script>
+	<script src='js/jquery.jkey.js' type='text/javascript' ></script>
+	<script src='js/ui.js' type='text/javascript' ></script>
+	<script src='js/functions.js' type='text/javascript' ></script>";
+
+
+?>
