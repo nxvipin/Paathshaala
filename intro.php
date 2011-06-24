5e15d0879f264adfc9e61d24011878038a8d3901
Jaseem Abid <jaseemabid@gmail.com>
2011-06-24 22:25:48 +0530

    Box layout page finished, no bugs noticed now

---
+++ intro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Paathshaala</title>
+
+<?php
+	include 'source.php';
+	echo $header;
+?>
+
+<link rel="stylesheet" href="css/storybox.css" />
+</head>
+<body>
+
+<div id='topbar'></div>
+	<div id='container'>
+		<?php echo $topNotLoggedIn; ?>
+	</div><!-- /container -->
+<?php echo $bottomBar; ?>
+<div id="bottombar"></div>
+
+<script src="js/ui.js" type="text/javascript"></script>
+<script src="js/storybox.js" type="text/javascript"></script>
+</body>
+</html>
