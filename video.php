fde6c6a35c7584940699f049c123712ccfc83641
Jaseem Abid <jaseemabid@gmail.com>
2011-06-27 22:01:35 +0530

    Updated video page with new template

--- video.php
+++ video.php
@@ -12,6 +12,8 @@
 </head>
 <body>
 <div id='topbar'></div>
+<div id='feedback'></div>
+<img src="pics/load.gif" id='loading'/ style='display:none;'>
 
 <div id='container'>
 <?php echo $topNotLoggedIn; ?>
