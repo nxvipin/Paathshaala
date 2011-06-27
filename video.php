65307befae3a4a469d302ebe77e4ebd4de2e7f2e
Jaseem Abid <jaseemabid@gmail.com>
2011-06-27 22:19:51 +0530

    Added inline css sheets and js to handle timeago plugin, comment box size and custom player

--- video.php
+++ video.php
@@ -8,7 +8,22 @@
 	include 'source.php';
 	echo $header;
 ?>
+<script src='js/video.js' type='text/javascript'></script>
+<link rel='stylesheet' href='css/video-js.css'>
 
+<script type='text/javascript'>
+$(document).ready(function() {
+		VideoJS.setupAllWhenReady();
+		$("time.timeago").timeago();
+		$('img.metaImage').error().attr('src','pics/default.png');
+	$('.commentBoxText').each(function() { /* Expand parent wrt to child */
+		var ht = $(this).height();
+		$(this).parent().height(ht);
+	});
+	var titleHt = $(".video_title").height(); /* Position side title at correct place */
+	$("span.sideMainTitle").css({"top" :-titleHt });
+});
+</script>
 </head>
 <body>
 <div id='topbar'></div>
