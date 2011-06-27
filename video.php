c848b7a0cedc96578e991261efb4152dcce771bf
Jaseem Abid <jaseemabid@gmail.com>
2011-06-27 21:58:11 +0530

    New video file from blank template

---
+++ video.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Paathshaala Intro</title>
+
+<?php
+
+	include 'source.php';
+	echo $header;
+?>
+
+</head>
+<body>
+<div id='topbar'></div>
+
+<div id='container'>
+<?php echo $topNotLoggedIn; ?>
+
+
+
+</div><!-- /container -->
+
+<?php echo $bottomBar; ?>
+<div id="bottombar"></div>
+<script src='js/ui.js' type='text/javascript' ></script>
+</body>
+</html>
