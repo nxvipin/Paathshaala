c914f9b404ed0dd1175aa14c169835d7af66c83d
Jaseem Abid <jaseemabid@gmail.com>
2011-07-01 02:04:18 +0530

    Implemented a sample profile page. Add space for recomended videos also

--- profile.php
+++ profile.php
@@ -9,6 +9,9 @@
 	echo $header;
 ?>
 <link rel='stylesheet' href='css/profile.css'>
+<link rel='stylesheet' href='css/storybox.css'>
+
+
 </head>
 <body>
 <div id='topbar'></div>
@@ -19,6 +22,24 @@
 <div id='container'>
 <?php echo $topNotLoggedIn; ?>
 
+<div id='editProfile'></div>
+
+<div id='profileBox'>
+	<img id='editProfileButton' src='pics/settings.png'/ >
+	<div id='snapShot'>
+		<img src='pics/me.png' style='width: 100%;' />
+		<span id='picUname'> Jaseem Abid </span>
+	</div>
+	
+	<div id='profileInfo'>
+		I am : <span id=''>Jaseem Abid </span><br />
+		here I am called : <span id='profileUsername'>jaseemabid</span><br />
+		Insti Roll : b090264cs (Helps us to give you better suggestions )<br />
+		One word to describe me : JavaScript Ninja <br />
+	</div>
+	
+</div>
+
 
 
 </div><!-- /container -->
@@ -26,5 +47,7 @@
 <?php echo $bottomBar; ?>
 <div id="bottombar"></div>
 <script src='js/ui.js' type='text/javascript' ></script>
+<script src='js/likedVideos.js' type='text/javascript' ></script>
+
 </body>
 </html>
