0e23bc9a80919a6b0490ccd0fe91184f224513eb
Vipin Nair <swvist@gmail.com>
2011-07-11 03:32:54 +0530

    Implemented sessions. Modified files to return the data of the active user.

--- json/uservideodislikes.json.php
+++ json/uservideodislikes.json.php
@@ -1,13 +1,17 @@
 <?php
 	/**
-	* The user ID is retrieved from a GET variable uid. Once sessions are implemented,
-	* User ID should be retrieved from session of current user. 
+	* JSON returning liked videos of the logged in user.
 	*/
 	include_once '../functions/class.content.php';
 	include_once '../functions/class.video.php';
 	include_once '../functions/class.user.php';
 	include_once '../functions/interface.php';
-	$uid=$_GET['uid'];
-	echo getUserVideoLikesJson($uid,-1);
+	if(checkSession()){
+		$uid=$_SESSION['uid'];	
+		echo getUserVideoLikesJson($uid,-1);
+	}
+	else{
+		redirect();
+	}
 	
 ?>
--- json/uservideouploads.json.php
+++ json/uservideouploads.json.php
@@ -1,13 +1,17 @@
 <?php
 	/**
-	* The user ID is retrieved from a GET variable uid. Once sessions are implemented,
-	* User ID should be retrieved from session of current user. 
+	* JSON containing all the video uploaded by the logged in user.
 	*/
 	include_once '../functions/class.content.php';
 	include_once '../functions/class.video.php';
 	include_once '../functions/class.user.php';
 	include_once '../functions/interface.php';
-	$uid=$_GET['uid'];
-	echo getUserUploadedVideosJson($uid);
+	if(checkSession()){
+		$uid=$_SESSION['uid'];	
+		echo getUserUploadedVideosJson($uid);		
+	}
+	else{
+		redirect();
+	}
 	
 ?>
