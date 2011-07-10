bd28126eea93dabec0299560b3c6d923db83ee72
Vipin Nair <swvist@gmail.com>
2011-07-11 04:34:28 +0530

    Modified activeuser JSON file to return logged in user details from session.

--- json/activeuser.json.php
+++ json/activeuser.json.php
@@ -1,5 +1,10 @@
 <?php
-	//JSON data for active user :-)
-	$uid=$_SESSION['uid'];
-	echo json_encode($uid);
+	//JSON data of the active user. Contains, uid, username, fullname, status and userpic.
+	include '../functions/functions.php';
+	if(checkSession()){
+		echo json_encode($_SESSION);
+	}
+	else{
+		redirect();
+	}
 ?>
