ba26736eb93f97b81565c9a042c832c1f702837f
Jaseem abid <jaseemabid@gmail.com>
2011-07-11 04:56:26 +0530

    Implemented comment delete and report spam with a crude version of inline getActiveUser() in video.php

--- js/comment.js
+++ js/comment.js
@@ -1,45 +1,70 @@
 /*
-	Codes to update the commentBox layout in the video page.
+	Codes to update the comments in the video page.
 	No need to call functions in the html since it is called in the js script.
 	Insert the script at the end of the page.
 */
 
 function updateComment() {
 	$("#loading").show();
-	$.getJSON( 'json/comment.json', function(myJsonObj) {
+	$.getJSON( 'json/comments.json.php', function(jsonObj) {
 		var commentDiv = '';
-		for (i in myJsonObj){
-			var myobj = myJsonObj[i];
-
+		var comments = jsonObj.rows;
+		var activeUser = getActiveUser();
+		function compare(a,b) {
+			if(a === b )
+				return "<span class='CommentCrossDelete'>X</span>";
+			else
+				return "<span class='CommentCrossReport'>X</span>";
+		}
+		for (i in comments){
+			var comment = comments[i].value;
+			var userId = comment[0];
 			var commentBox = 
-			"<div class='commentBox' cId='" + myobj.commentid +"' >" +
-				"<div class='commentBoxImage'> <img src='" + myobj.authorpic + "' class='fitin' /> </div>" +
+			"<div class='commentBox' cId='" + comments[i]["id"] +"' >" +
+				"<div class='commentBoxImage'> <img src='" + comment[3] + "' class='fitin' /> </div>" +
 				"<div class='commentBoxText'>" +
-					"<span class='delComCross'>X</span>" +
-					"<span class='commentTitle'>"+ myobj.author +"</span>" +
-					"<time class='timeago' datetime='" + myobj.timestamp + "'></time>" +
-					"<div class='data'>" + myobj.data + "</div>" +
+					compare(activeUser, userId ) +
+					"<span class='commentTitle'>"+ comment[1] +"</span>" +
+					"<time class='timeago' datetime='" + comment[4] + "'></time>" +
+					"<div class='data'>" + comment[2] + "</div>" +
 				"</div>" +
 			"</div>";
 			commentDiv = commentDiv + commentBox;
 		}
-
 		$('div.commentWarp').html(commentDiv);
 	}).complete(function() {
 
 /* Handler for the 'X' on the comment box */
 
-		$("span.delComCross").click(function() {
+		$("span.CommentCrossDelete").click(function() {
 			var cross = $(this);
 			var click = cross.parent().parent(); // Click is the whole comm div
 			var commentId = click.attr('cId');
 			var temp = cross.parent().children(".data"); // <div class='data'>
 			var origContent = temp.html();
-			temp.html("<div id='delConfirmRespText'>You really wanna delete the coment ? <br/> <span class='delYesNo' id='delYes'> Yes </span> <span class='delYesNo' id='delNo'> No </span></div>");
+			temp.html("<div id='delConfirmRespText'>You really want to delete this comment ? <br/> <span class='delYesNo' id='delYes'> Yes </span> <span class='delYesNo' id='delNo'> No </span></div>");
 			cross.css({'color':'#f9f9f9' , 'cursor':'url(pics/arrow.png), auto' });
 			$('span#delYes').click(function() {
 				click.fadeOut("slow");
-				$.post("delC.php", { comId: commentId } );
+				$.post("response/deletecomment.response.php", { comId: commentId } );
+			});
+			$('span#delNo').click(function() {
+				temp.html(origContent);
+				cross.css({'color':'#555555' , 'cursor':'url(pics/pointer.png), auto' });
+			});
+		});
+
+		$("span.CommentCrossReport").click(function() {
+			var cross = $(this);
+			var click = cross.parent().parent(); // Click is the whole comm div
+			var commentId = click.attr('cId');
+			var temp = cross.parent().children(".data"); // <div class='data'>
+			var origContent = temp.html();
+			temp.html("<div id='delConfirmRespText'>You really want to report this comment ? <br/> <span class='delYesNo' id='delYes'> Yes </span> <span class='delYesNo' id='delNo'> No </span></div>");
+			cross.css({'color':'#f9f9f9' , 'cursor':'url(pics/arrow.png), auto' });
+			$('span#delYes').click(function() {
+				click.fadeOut("slow");
+				$.post("response/reportcomment.response.php", { comId: commentId } );
 			});
 			$('span#delNo').click(function() {
 				temp.html(origContent);
