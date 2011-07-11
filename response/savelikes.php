d02c44827176d1111971d29d6fbcdeaf0c3bec09
Vipin Nair <swvist@gmail.com>
2011-07-11 22:50:15 +0530

    Changed file name to savelikes, and fixed session bug.

--- response/savelikes.php
+++ response/savelikes.php
@@ -1,11 +1,13 @@
 <?php
 	/**
-	* Accepts video likes from front end and stores in database.
+	* Accepts Content likes from front end and stores in database.
 	* TODO: Implement Sessions.
 	*/
 	include '../functions/class.activity.php';
 	$cid=$_POST['cid'];
 	$value=$_post['value'];
 	$uid=$_SESSION['uid'];
-	activity::like($cid,$uid,$value);
+	if($uid){
+		activity::like($cid,$uid,$value);
+	}
 ?>
