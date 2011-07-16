2d619ec739fa8789079b37253e544a68af2c22eb
Vipin Nair <swvist@gmail.com>
2011-07-17 03:27:24 +0530

    Sends the comment to CouchDB

---
+++ response/submitcomment.php
@@ -0,0 +1,16 @@
+<?php
+
+	/**
+	* Submits the comment to CouchDB.
+	*/
+	include_once '../functions/class.comment.php';
+	session_start();
+	if(isset($_SESSION['uid']) && isset($_POST['comment']) && isset($_POST['cid'])){
+		$_POST['uid']=$_SESSION['uid'];
+		$_POST['uname']=$_SESSION['username'];
+		$_POST['timestamp']= date("Y-m-d h-i-s");
+		$c = new comment();
+		echo $c->post($_POST);
+	}
+
+?>
