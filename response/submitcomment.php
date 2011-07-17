0acac2bf7bec3ba967ed2dfac646cf9b2d754511
Vipin Nair <swvist@gmail.com>
2011-07-17 07:16:10 +0530

    Modified comment timestamp to include timezone because of a chrome bug.

--- response/submitcomment.php
+++ response/submitcomment.php
@@ -8,7 +8,7 @@
 	if(isset($_SESSION['uid']) && isset($_POST['comment']) && isset($_POST['cid'])){
 		$_POST['uid']=$_SESSION['uid'];
 		$_POST['uname']=$_SESSION['username'];
-		$_POST['timestamp']= date("Y-m-d h:i:s");
+		$_POST['timestamp']= date("Y-m-d H:i:s+0530");
 		$c = new comment();
 		echo $c->post($_POST);
 	}
