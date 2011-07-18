48aaa6e4ee4e5929e59508648589c3fff4b4b841
Jaseem Abid <jaseemabid@gmail.com>
2011-07-19 02:45:22 +0530

    Returns the json data of a video when cid is passed as a get variable [video]. Likes data missing

---
+++ json/video.json.php
@@ -0,0 +1,7 @@
+<?php
+	include_once '../functions/class.content.php';
+	include_once '../functions/class.video.php';
+	include_once '../functions/class.user.php';
+	include_once '../functions/interface.php';
+	echo getVideoJson($_GET['video']);
+?>
