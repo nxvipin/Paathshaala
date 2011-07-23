50aab8e36d56f249a60f1122559832dbecf828b4
Vipin Nair <swvist@gmail.com>
2011-07-23 21:18:27 +0530

    Modified join to check if email already exists.

--- response/join.php
+++ response/join.php
@@ -5,9 +5,21 @@
 	
 	include_once '../functions/class.user.php';
 	include_once '../functions/class.activity.php';
-	
-	$u = new user($_POST['uname'],$_POST['upass'],$_POST['ufullname'],$_POST['uroll'],$_POST['uemail']);
-	activity::login($_POST['uname'],$_POST['upass']);
-	header("Location: profile.php");
+	$status=array();
+	if(user::checkEmailExists($_POST['ufullname'])==1){
+		$status['status']=2;
+		return json_encode($status);
+	}
+	else{
+		$u = new user($_POST['uname'],$_POST['upass'],$_POST['ufullname'],$_POST['uroll'],$_POST['uemail']);
+		if(!$u){
+			$status['status']=0;
+			exit;
+		}
+		else{
+			activity::login($_POST['uname'],$_POST['upass']);
+			header("Location: ../profile.php");
+		}
+	}
 
 ?>
