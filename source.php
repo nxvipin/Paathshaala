b1b8eb4b1d2ad279a3a5e155c712347f1bc304fe
Jaseem Abid <jaseemabid@gmail.com>
2011-07-24 20:48:23 +0530

    Edited form.join variables

--- source.php
+++ source.php
@@ -39,18 +39,16 @@ $topNotLoggedIn = "<div id='top'>
 	<button type='submit' class='tickButton'><img src='pics/tick.png'></button>
 </form>
 
-<form class='join' action='response/join.php' method='post'>
+<form class='join' action='response/join.php' method='get'>
 		<div class='joinMessage'>Enter your credentials</div>
 		<img src='pics/user.png'> <input required type='text' id='fname' name='fname' placeholder='Full Name' /> <img src=''><br />
-		<img src='pics/users.png'> <input required type='text' id='uname' name='uname' placeholder='Username' /> <img src=''> <span class='check'>Check</span> <br />
-		<img src='pics/mail.png'> <input required type='email' id='email' name='email' placeholder='Email'/> <img src=''> <br />
+		<img src='pics/users.png'> <input required type='text' id='username' name='uname' placeholder='Username' /> <img src=''> <br />
+		<img src='pics/mail.png'> <input required type='email' id='email' name='email' placeholder='Email'/> <img src=''><br />
 		<img src='pics/roll.png'> <input required type='text' id='roll' name='roll' placeholder='Roll Number' /> <img src=''> <br />
 		<img src='pics/key.png'> <input required type='password' id='pass1' name='pass1' placeholder='Password' /> <img src=''> <br />
 		<img src='pics/key.png'> <input required type='password' id='pass2' name='pass2' placeholder='Password again'/> <img src=''> <br />
-		<input type='hidden' name='page' value='".$_SERVER['PHP_SELF']."'>
-		<img id='joinButton' src='pics/tick.png' style='float:right; margin-right:5px'>
+		<button id='joinButton' type='button'>Join</button>
 </form>
-
 </div> <!-- /top -->";
 	}
 	else{
