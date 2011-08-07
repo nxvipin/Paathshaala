13e6ec94f8c11055ded2e9b5f3737fb87d910ddd
Vipin Nair <swvist@gmail.com>
2011-08-07 19:06:47 +0530

    Bugfix for logged out user.

--- response/submitfeedback.php
+++ response/submitfeedback.php
@@ -4,17 +4,16 @@
 	*/
 	session_start();
 	include_once '../functions/class.activity.php';
-	$uid=NULL;
+	$uid=0;
 	if(isset($_POST['type']) && isset($_POST['desc'])){
 		if(isset($_SESSION['uid'])){
 			$uid=$_SESSION['uid'];
 		}
-		else{
-			$uid=0;
+		$res=activity::feedback($_SERVER['REMOTE_ADDR'],$uid,$_POST['type'],$_POST['desc']);
+		if($res){
+			echo "Thanks for the feedback.";
 		}
-		activity::feedback($_SERVER['REMOTE_ADDR'],$uid,$_POST['type'],$_POST['desc']);
 	}
 	
-	// Need response as a proper string. Var name : status, type json, like 'Feedback Submitted sucessfully' or 'Failed !'
 
 ?>
