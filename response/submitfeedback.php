431f0b594a7b09ff1adb41aee768fb91a62eb635
Vipin Nair <swvist@gmail.com>
2011-08-07 19:17:48 +0530

    Fixed logged out feedback bug.

--- response/submitfeedback.php
+++ response/submitfeedback.php
@@ -5,15 +5,14 @@
 	session_start();
 	include_once '../functions/class.activity.php';
 	$uid=0;
-	if(isset($_POST['type']) && isset($_POST['desc'])){
-		if(isset($_SESSION['uid'])){
-			$uid=$_SESSION['uid'];
-		}
-		$res=activity::feedback($_SERVER['REMOTE_ADDR'],$uid,$_POST['type'],$_POST['desc']);
-		if($res){
-			echo "Thanks for the feedback.";
-		}
+	if($_SESSION['uid']){
+		$uid=$_SESSION['uid'];
 	}
-	
+	$res=activity::feedback($_SERVER['REMOTE_ADDR'],$uid,$_POST['type'],$_POST['desc']);
+	if($res){
+		$status=array();
+		$status['status']="Thanks for the feedback.";
+		echo json_encode($status);
+	}
 
 ?>
