5e2148f23d4d7864560cfc502b25c915d258f72b
Jaseem Abid <jaseemabid@gmail.com>
2011-11-08 23:38:18 +0530

    Sample json updated with small bug fix

--- json/getnewvideo.json.php
+++ json/getnewvideo.json.php
@@ -1,6 +1,6 @@
 {
 	"path"		: "http://192.168.74.190/Paathshaala/content/bbb.ogv",
 	"poster"	: "http://192.168.74.190/Paathshaala/content/bbb.ogv",
-	"series"	: "Send null for now " 
+	"series"	: "Send null for now " ,
 	"uname"		: "Jaseem Abid"
 }
