91e4b8c2ef845ba6b82fe831af2e2d002691b631
Jaseem Abid <jaseemabid@gmail.com>
2011-11-20 11:43:53 +0530

    A small data URI Experiment

--- source.php
+++ source.php
@@ -17,6 +17,7 @@ $feedback = "<img src='pics/feedback.png' alt='feedback button' title='feedback 
 
 $piwik = "<script type=\"text/javascript\">var pkBaseURL =\"http://192.168.5.27/piwik/\"; document.write(unescape(\"%3Cscript src=\'\" + pkBaseURL + \"piwik.js\' type=\'text/javascript\'%3E%3C/script%3E\"));</script><script type=\"text/javascript\">try {var piwikTracker = Piwik.getTracker(pkBaseURL + \"piwik.php\", 1);piwikTracker.trackPageView();piwikTracker.enableLinkTracking();} catch( err ) {} </script>";
 
+$nullImage="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9sJCgcqHkarI5cAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAAHElEQVQ4y2P8//8/AyWAiYFCMGrAqAGjBgwWAwBjmgMd7D3zQQAAAABJRU5ErkJggg==";
 
 	if(!isset($_SESSION['uid'])){
 $topBar = "<div id='top'>
@@ -43,12 +44,12 @@ $topBar = "<div id='top'>
 
 <form class='join' action='response/join.php' method='get'>
 		<div class='joinMessage'>Enter your credentials</div>
-		<img src='pics/user.png'> <input required type='text' id='fname' placeholder='Full Name' /> <img src='pics/null.png'><br />
-		<img src='pics/users.png'> <input required type='text' id='username' placeholder='Username' /> <img src='pics/null.png'> <br />
-		<img src='pics/mail.png'> <input required type='email' id='email' placeholder='Email'/> <img src='pics/null.png'><br />
-		<img src='pics/roll.png'> <input required type='text' id='roll' placeholder='Roll Number' /> <img src='pics/null.png'> <br />
-		<img src='pics/key.png'> <input required type='password' id='pass1' placeholder='Password' /> <img src='pics/null.png'> <br />
-		<img src='pics/key.png'> <input required type='password' id='pass2' placeholder='Password again'/> <img src='pics/null.png'> <br />
+		<img src='pics/user.png'> <input required type='text' id='fname' placeholder='Full Name' /> <img src='.$nullImage.'><br />
+		<img src='pics/users.png'> <input required type='text' id='username' placeholder='Username' /> <img src='.$nullImage.'> <br />
+		<img src='pics/mail.png'> <input required type='email' id='email' placeholder='Email'/> <img src='.$nullImage.'><br />
+		<img src='pics/roll.png'> <input required type='text' id='roll' placeholder='Roll Number' /> <img src='.$nullImage.'> <br />
+		<img src='pics/key.png'> <input required type='password' id='pass1' placeholder='Password' /> <img src='.$nullImage.'> <br />
+		<img src='pics/key.png'> <input required type='password' id='pass2' placeholder='Password again'/> <img src='.$nullImage.'> <br />
 		<button id='joinButton' type='button'>Join</button>
 </form>
 </div> <!-- /top -->";
