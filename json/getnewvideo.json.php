94a512e2c3f5f61968dbdc78c02cef6e63d65331
Vipin Nair <swvist@gmail.com>
2011-12-12 00:03:30 +0530

    Returns a new video JSON if the user is logged in

--- json/getnewvideo.json.php
+++ json/getnewvideo.json.php
@@ -1,6 +1,13 @@
-{
-	"path"		: "http://192.168.74.190/Paathshaala/content/bbb.ogv",
-	"poster"	: "http://192.168.74.190/Paathshaala/content/bbb.ogv",
-	"series"	: "Send null for now " ,
-	"uname"		: "Jaseem Abid"
-}
+<?php
+	/**
+	* JSON returning disliked videos of a logged in user.
+	*/
+	include_once '../functions/class.content.php';
+	include_once '../functions/class.video.php';
+	include_once '../functions/class.user.php';
+	include_once '../functions/interface.php';
+	session_start();
+	if(checkSession()){
+		echo getNewVideoJson($_SESSION['fullname']);
+	}
+?>
